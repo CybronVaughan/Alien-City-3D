f703663aba565a2697d6536cafbd4db133ea0270
Gabriel de Lucca Vieira <bielluccabmv@gmail.com>
2018-09-24 15:13:33 -0300

    Commit 01
      - Terminado o labirinto.
      - Ajustado Network Animator, Network Transform, Network Identity e Player Controller.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
 [RequireComponent(typeof(CharacterController))]
-public class PlayerController : MonoBehaviour {
+public class PlayerController : NetworkBehaviour {
 
 	public float MoveSpeed;
 	public float RotationSpeed;
 	CharacterController cc;
-	private Animator anim;
+	private NetworkAnimator anim;
 	protected Vector3 gravidade = Vector3.zero;
 	protected Vector3 move = Vector3.zero;
 	private bool jump = false;
@@ -16,14 +17,22 @@ public class PlayerController : MonoBehaviour {
 	
 	void Start()
 	{
+        if (!isLocalPlayer)
+        {
+            return;
+        }
 		cc = GetComponent<CharacterController> ();
-		anim = GetComponent<Animator>();
+		anim = GetComponent<NetworkAnimator>();
 		anim.SetTrigger("Parado");
 	}
 
 	void Update()
 	{
-		Vector3 move = Input.GetAxis ("Vertical") * transform.TransformDirection (Vector3.forward) * MoveSpeed;
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        Vector3 move = Input.GetAxis ("Vertical") * transform.TransformDirection (Vector3.forward) * MoveSpeed;
 		transform.Rotate (new Vector3 (0, Input.GetAxis ("Horizontal") * RotationSpeed * Time.deltaTime, 0));
 		
 		if (!cc.isGrounded) {
@@ -34,7 +43,7 @@ public class PlayerController : MonoBehaviour {
 			gravidade = Vector3.zero;
 			if(jump)
 			{
-				gravidade.y = 6f;
+				gravidade.y = 3f;
 				jump = false;
 			}
 		}
